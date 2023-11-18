2a59366ea15b3aad5ce30eb3f1a2d6bc0627a91d
YeongHyeon Jeong <dudgus7495@gmail.com>
2023-11-18 15:04:49 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+
 
 function App() {
+
+
+  const [ city, setCity ] = useState("Toronto")
+
+  const apiKey = "f674e616810a6019adbaa5b42a09cdbf"
+  
+  // const url = ""
+  useEffect(() => {
+     axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
+    .then((response) => {
+      console.log(response.data)
+      setCity(response.data)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  }, [])
+ 
+
+
+
+  // const cityName = () => {
+
+  // }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input />
+      {/* <button /> */}
     </div>
   );
 }
