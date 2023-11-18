7dbb5a32e6976c769a51674cf82bde4363598da4
Tsering Dhondup <tyddhondup88@gmail.com>
2023-11-18 16:50:26 -0500

    get request finalised and begin importing data into Weather component.

--- src/App.js
+++ src/App.js
@@ -1,45 +1,46 @@
-import './App.css';
-import React, { useState, useEffect } from 'react';
-import axios from 'axios'
-import Weather from './components/Weather';
-
-
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Weather from "./components/Weather/Weather";
+import Search from "./components/Search/Search";
+import Header from "./components/Header/Header";
+// api key
+import { apiKey } from "./components/utilities/api";
 function App() {
+  const [city, setCity] = useState(""); // name
+  const [weatherData, setWeatherData] = useState("");
+  const [mainData, setMainData] = useState("");
+  const [wind, setWind] = useState("");
+  const [searchValue, setSearchValue] = useState("");
 
+  const getData = () => {
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+      )
+      .then((response) => {
+        setWeatherData(response.data.weather[0]);
+        setMainData(response.data.main);
+        setWind(response.data.wind);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-  const [ city, setCity ] = useState("Toronto")
-
-  const apiKey = "f674e616810a6019adbaa5b42a09cdbf"
-  
-  // const url = ""
-  useEffect(() => {
-    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
-    .then((response) => {
-      console.log(response.data)
-      setCity(response.data)
-    })
-    .catch((error) => {
-      console.log(error)
-    })
-  }, [])
-
-
-
-
-  // const cityName = () => {
-
-  // }
-
-
+  const handleCity = (cityName) => {
+    setCity(cityName);
+  };
   return (
     <div className="App">
-      <div className=''>
-        <input />
-        {/* <button /> */}  
-      </div>
-      <Weather 
-      humidity={city.weather}/>
-      
+      <Header />
+      <Search
+        getData={getData}
+        searchValue={searchValue}
+        handleCity={handleCity}
+      />
+      <Weather weatherData={weatherData} mainData={mainData} wind={wind} />
     </div>
   );
 }
---
+++ src/components/utilities/api.js
@@ -0,0 +1,1 @@
+export const apiKey = "f674e616810a6019adbaa5b42a09cdbf";
