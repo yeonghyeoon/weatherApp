a699da3ce9f7432f88b5b424b53aba393f5184a9
YeongHyeon Jeong <dudgus7495@gmail.com>
2023-11-19 02:53:10 -0500

    added a function for hitting enter key in the input

--- src/App.js
+++ src/App.js
@@ -35,6 +35,7 @@ function App() {
   };
 
   const handleCity = (cityName) => {
+  
     setCity(cityName);
   };
 
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -2,18 +2,27 @@ import "./Search.css";
 import searchIcon from "../../assets/icons/search.svg"
 
 const Search = (props) => {
+
+  const searchLocation = (e) => {
+    if(e.key === 'Enter') {
+      props.getData();
+      
+    }
+    return;
+  }
+
   return (
-    <form className="form-search">
+    <div className="form-search">
       <input 
         className="input-location"
         placeholder="Search Location"
         onChange={(event) => {
           props.handleCity(event.target.value);
         }}
-        // onKeyDown={props.getData}
+        onKeyDown={searchLocation}
       />
       <button type="button" className="search-btn" onClick={props.getData}><img src={searchIcon} /></button>
-    </form>
+    </div>
   );
 };
 export default Search;
