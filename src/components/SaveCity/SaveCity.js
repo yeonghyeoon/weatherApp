b6730b4b3f607c7f297a9e754ed17deacc3d1aba
xrhurtado <xrhurtado@gmail.com>
2023-11-20 14:37:28 -0500

    styling getting closer

--- src/components/SaveCity/SaveCity.js
+++ src/components/SaveCity/SaveCity.js
@@ -4,12 +4,14 @@ const SaveCity = (props) => {
   let saveCityEl;
   if (props.saveCityData) {
     saveCityEl = props.saveCityData.map((saveCity) => (
-      <div key={saveCity.id}>
-        <h3 className="saved__cities-city">{saveCity.city}</h3>
-        <p className="saved__cities-temperature">{saveCity.temp}°C</p>
-        <p className="saved__cities-description">{saveCity.description}</p>
-        <p>{saveCity.windSpeed}</p>
-        <p>{saveCity.windDeg}</p>
+      <div className="saved__cities-card" key={saveCity.id}>
+        <img className="saved__cities-image" src="https://bmcdn.nl/assets/weather-icons/v3.0/fill/svg/sun-hot.svg" />
+        <div className="saved__cities-data">
+          <h3 className="saved__cities-city">{saveCity.city}</h3>
+          <p className="saved__cities-temperature">{saveCity.temp}°C</p>
+          <p className="saved__cities-description">{saveCity.description}</p>
+          <p>{saveCity.windSpeed} km/h</p>
+        </div>
       </div>
     ));
   }
@@ -17,7 +19,7 @@ const SaveCity = (props) => {
   return (
     <div className="saved__cities">
       <h1 className="saved__cities-header">Saved Cities</h1>
-      <div className="saved__cities-card">{saveCityEl}</div>
+      <div className="saved__cities-section">{saveCityEl}</div>
     </div>
   );
 };
