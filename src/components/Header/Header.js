9a03421a54802fe454f724bd44c4388aef009025
YeongHyeon Jeong <dudgus7495@gmail.com>
2023-11-19 20:05:13 -0500

    changed icons

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,18 @@
 import shareLogo from "../../assets/icons/send.png";
 import gpsLogo from "../../assets/icons/gps.svg";
 import './Header.css';
+import menuLogo from "../../assets/icons/menu.png";
+import addingLogo from "../../assets/icons/plus.png";
 
 const Header = () => {
+  
+  
+
   return (
     <div className="header">
       <div className="header__logo">
-        <img src={shareLogo} className="shareLogo" alt="send-logo" />
-        <img src={gpsLogo} className="gpsLogo" alt="gps-logo" />  
+        <img src={menuLogo} className="header__logo-menuLogo" alt="send-logo" />
+        <button type="button" className="header__btn"><img src={addingLogo} className="header__logo-addingLogo" alt="gps-logo" /></button>  
       </div>
       
       {/* <h1>Weather App</h1> */}
